a1d74fd8f0069b6821760253ac88f4f8909f677c
Daniel Brown <d.t.brown@outlook.com>
2016-02-06 21:08:30 +0000

    Transpiled code shouldnt be in source control

--- app/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _log = require('./log');
-
-var _log2 = _interopRequireDefault(_log);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_log2.default.info('CS Blogs Feed Aggregator Started');
