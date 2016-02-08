febbceea775e0d9f64f08518f28a11404fcad120
Daniel Brown <d.t.brown@outlook.com>
2016-02-08 19:20:29 +0000

    Added user model with same fields as old system.
    Also added code to sync the database (ensure tables are created) and
    then grab the parts of the user model we need for feed downloading

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 import log from './log';
+import database from './database';
+import User from './database/models/user';
 
 log.info('CS Blogs Feed Aggregator Started');
+
+database.sync().then(() => {
+  User.findAll({
+    attributes: ['id', 'blogFeedURI']
+  }).forEach(user => {
+    log.info({ user }, 'User loaded from database');
+  });
+});
