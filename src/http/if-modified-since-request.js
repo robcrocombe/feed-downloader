46f5a141f13301fcb8d19124fa78bbf40d17566c
Daniel Brown <d.t.brown@outlook.com>
2016-02-11 22:27:38 +0000

    Improved logging and error handling. Use inbuilt dateTime functions for UTC.

--- src/http/if-modified-since-request.js
+++ src/http/if-modified-since-request.js
@@ -1,22 +1,38 @@
 import request from 'request';
-import moment from 'moment';
-
-function dateTimeToRFC822Format(dateTime) {
-  return moment(dateTime).format('ddd, DD MMM YYYY HH:mm:ss ZZ');
-}
+import log from '../log';
 
 export default function requestIfModifiedSince(uri, dateTime = null) {
   return new Promise((resolve, reject) => {
-    const headerDate = dateTimeToRFC822Format(dateTime);
     const requestOptions = {
       uri,
-      headers: { 'If-Modified-Since': headerDate }
+      headers: { 'If-Modified-Since': dateTime.toUTCString() }
     };
+
     request(requestOptions, (error, response, body) => {
       if (error) {
-        reject(error);
+        log.error({ uri, error },
+                  'HTTP Request for resource resulted in error');
+        reject(new Error('HTTP request unsuccessful'));
+      }
+
+      if ((response.statusCode !== 200 && response.statusCode !== 304)) {
+        log.info({ uri, response }, 'HTTP Request returned non-success status code');
+        reject(new Error('Non-success status code'));
       } else {
-        resolve(body);
+        if (response.statusCode === 304) {
+          log.info({ uri, lastModified: dateTime.toUTCString() },
+                   'Requested resource has not been modified since lastModified date');
+          resolve({ modified: false });
+        } else {
+          const newLastModifiedDateTime = new Date(response.headers['last-modified']);
+
+          log.info({ uri,
+                     oldLastModified: dateTime.toUTCString(),
+                     newLastModified: newLastModifiedDateTime.toUTCString()
+                    }, 'Requested resource has been modified since oldLastModified date');
+
+          resolve({ modified: true, newLastModifiedDateTime, body });
+        }
       }
     });
   });
