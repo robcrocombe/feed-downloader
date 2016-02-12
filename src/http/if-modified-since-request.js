6e6e2afd524baf6d1f2e43aefeafcb9a4fb8a7ff
Daniel Brown <d.t.brown@outlook.com>
2016-02-12 23:56:52 +0000

    Added timeout & fixed type error being thrown when request.js had an error

--- src/http/if-modified-since-request.js
+++ src/http/if-modified-since-request.js
@@ -1,20 +1,22 @@
 import request from 'request';
 import log from '../log';
 import check from 'check-types';
+import config from '../../config/config.json';
 
-export default function httpRequestIfModifiedSince(uri, dateTime = null) {
+export default function httpRequestIfModifiedSince(uri, dateTime) {
   return new Promise((resolve, reject) => {
     if (!dateTime || check.instance(dateTime, Date.prototype)) {
       reject(new Error('Valid DateTime required'));
     }
 
     if (!check.nonEmptyString(uri)) {
-      reject(new Error('URI required'));
+      reject(new Error('Valid URI required'));
     }
 
     const requestOptions = {
       uri,
-      headers: { 'If-Modified-Since': dateTime.toUTCString() }
+      headers: { 'If-Modified-Since': dateTime.toUTCString() },
+      timeout: config.http.timeoutInMilliseconds
     };
 
     request(requestOptions, (error, response, data) => {
@@ -22,25 +24,25 @@ export default function httpRequestIfModifiedSince(uri, dateTime = null) {
         log.error({ uri, error },
                   'HTTP Request for resource resulted in error');
         reject(new Error('HTTP request unsuccessful'));
-      }
-
-      if ((response.statusCode !== 200 && response.statusCode !== 304)) {
-        log.info({ uri, response }, 'HTTP Request returned non-success status code');
-        reject(new Error('Non-success status code'));
       } else {
-        if (response.statusCode === 304) {
-          log.info({ uri, lastModified: dateTime.toUTCString() },
-                   'Requested resource has not been modified since lastModified date');
-          resolve({ modified: false });
+        if ((response.statusCode !== 200 && response.statusCode !== 304)) {
+          log.info({ uri, response }, 'HTTP Request returned non-success status code');
+          reject(new Error('Non-success status code'));
         } else {
-          const newLastModifiedDateTime = new Date(response.headers['last-modified']);
+          if (response.statusCode === 304) {
+            log.info({ uri, lastModified: dateTime.toUTCString() },
+                    'Requested resource has not been modified since lastModified date');
+            resolve({ modified: false });
+          } else {
+            const newLastModifiedDateTime = new Date(response.headers['last-modified']);
 
-          log.info({ uri,
-                     oldLastModified: dateTime.toUTCString(),
-                     newLastModified: newLastModifiedDateTime.toUTCString()
-                    }, 'Requested resource has been modified since oldLastModified date');
+            log.info({ uri,
+                       oldLastModified: dateTime.toUTCString(),
+                       newLastModified: newLastModifiedDateTime.toUTCString()
+                     }, 'Requested resource has been modified since oldLastModified date');
 
-          resolve({ modified: true, newLastModifiedDateTime, data });
+            resolve({ modified: true, newLastModifiedDateTime, data });
+          }
         }
       }
     });
--- src/http/request.js
+++ src/http/request.js
@@ -1,21 +1,27 @@
 import request from 'request';
 import log from '../log';
+import config from '../../config/config.json';
+import check from 'check-types';
 
 export default function httpRequest(uri) {
   return new Promise((resolve, reject) => {
-    request(uri, (error, response, data) => {
+    request({ uri, timeout: config.http.timeoutInMilliseconds }, (error, response, data) => {
+      if (!check.nonEmptyString(uri)) {
+        reject(new Error('Valid URI required'));
+      }
+
       if (error) {
         log.error({ uri, error },
                   'HTTP Request for resource resulted in error');
         reject(new Error('HTTP request unsuccessful'));
-      }
-
-      if ((response.statusCode !== 200)) {
-        log.info({ uri, response }, 'HTTP Request returned non-success status code');
-        reject(new Error('Non-success status code'));
       } else {
-        const lastModifiedDateTime = new Date(response.headers['last-modified']);
-        resolve({ lastModified: lastModifiedDateTime, data });
+        if ((response.statusCode !== 200)) {
+          log.info({ uri, response }, 'HTTP Request returned non-success status code');
+          reject(new Error('Non-success status code'));
+        } else {
+          const lastModifiedDateTime = new Date(response.headers['last-modified']);
+          resolve({ lastModified: lastModifiedDateTime, data });
+        }
       }
     });
   });
