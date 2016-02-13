afbdeb176fa41ebddb4ff8824d9f9dbe41632d74
Daniel Brown <d.t.brown@outlook.com>
2016-02-13 22:54:01 +0000

    Added a handler function which is called when an event occurs.
    Also fixed up promise code to be a bit nicer

--- src/main.js
+++ src/main.js
@@ -2,14 +2,23 @@ import log from './log';
 import database from './database';
 import User from './database/models/user';
 
-log.info('CS Blogs Feed Aggregator Started');
+export function start() {
+  log.info('CS Blogs Feed Aggregator Started');
 
-database.sync().then(() => {
-  User.findAll({
-    attributes: ['id', 'blogFeedURI']
-  }).then(users => {
-    users.forEach(user => {
-      log.info({ user }, 'User loaded from database');
+  database.sync()
+    .then(() =>
+      User.findAll({
+        attributes: ['id', 'blogFeedURI']
+      }))
+    .then(users => {
+      users.forEach(user => {
+        log.info({ user }, 'User loaded from database');
+      });
     });
-  });
-});
+}
+
+export function awsLambdaHandle(event, context) {
+  log.info({ event, context }, 'AWS Lamba Event Triggered Launch');
+}
+
+start();
