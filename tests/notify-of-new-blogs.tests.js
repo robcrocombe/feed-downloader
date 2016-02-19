c629968ab9bf62ffdef1e818d952311c557e997f
Daniel Brown <d.t.brown@outlook.com>
2016-02-19 16:40:04 +0000

    Cleaned up weird 1 space indentation of tests. Added tests for invalid parameter passed to notifyOfNewBlogs() function

--- tests/notify-of-new-blogs.tests.js
+++ tests/notify-of-new-blogs.tests.js
@@ -1,105 +1,115 @@
- import newPostNotification from '../src/notifications/notify-of-new-blogs';
+import newPostNotification from '../src/notifications/notify-of-new-blogs';
 
- describe('sns', () => {
-   describe('notifyOfBlogUpdatesSNS()', () => {
-     it('should post correct message for a single new post', done => {
-       newPostNotification([
-         {
-           firstName: 'Danny',
-           lastName: 'Brown',
-           newPosts: [
-             { title: 'The new CS Blogs platform!' }
-           ]
-         }
-       ])
-         .then(() => done())
-         .catch(done);
-     });
+const expect = global.expect;
 
-     it('should post correct message for 2 posts by one author', done => {
-       newPostNotification([
-         {
-           firstName: 'Danny',
-           lastName: 'Brown',
-           newPosts: [
-             { title: 'Graduating from York' },
-             { title: 'HackTrain 2.0' }
-           ]
-         }
-       ])
-         .then(() => done())
-         .catch(done);
-     });
+describe('notify-of-new-blogs', () => {
+  describe('notifyOfNewBlogs()', () => {
+    it('should throw if no blog updates provided', () =>
+      expect(newPostNotification([])).to.be.rejectedWith('Error: Valid array of users with new posts required')
+    );
 
-     it('should post correct message for >2 posts by one author', done => {
-       newPostNotification([
-         {
-           firstName: 'Danny',
-           lastName: 'Brown',
-           newPosts: [
-             { title: 'Graduating from York' },
-             { title: 'HackTrain 2.0' },
-             { title: 'Centre for Computing History' }
-           ]
-         }
-       ])
-         .then(() => done())
-         .catch(done);
-     });
+    it('should throw if non-array passed as parameter', () =>
+      expect(newPostNotification('notAnArray')).to.be.rejectedWith('Error: Valid array of users with new posts required')
+    );
 
-     it('should post correct message for multiple posts by 2 authors', done => {
-       newPostNotification([
-         {
-           firstName: 'Danny',
-           lastName: 'Brown',
-           newPosts: [
-             { title: 'Graduating from York' },
-             { title: 'HackTrain 2.0' }
-           ]
-         },
-         {
-           firstName: 'Rob',
-           lastName: 'Crocombe',
-           newPosts: [
-             { title: 'Readability: Slack vs HipChat' },
-             { title: 'A walk through blog technologies' }
-           ]
-         }
-       ])
+    it('should post correct message for a single new post', done => {
+      newPostNotification([
+        {
+          firstName: 'Danny',
+          lastName: 'Brown',
+          newPosts: [
+            { title: 'The new CS Blogs platform!' }
+          ]
+        }
+      ])
         .then(() => done())
         .catch(done);
-     });
+    });
 
-     it('should post correct message for multiple posts by >2 authors', done => {
-       newPostNotification([
-         {
-           firstName: 'Danny',
-           lastName: 'Brown',
-           newPosts: [
-             { title: 'Graduating from York' },
-             { title: 'HackTrain 2.0' }
-           ]
-         },
-         {
-           firstName: 'Alex',
-           lastName: 'Pringle',
-           newPosts: [
-             { title: 'Cycling out of the north' },
-             { title: 'Touring the North' },
-             { title: 'End Of The Tour' }
-           ]
-         },
-         {
-           firstName: 'Rob',
-           lastName: 'Crocombe',
-           newPosts: [
-             { title: 'Readability: Slack vs HipChat' },
-             { title: 'A walk through blog technologies' }
-           ]
-         }
-       ])
+    it('should post correct message for 2 posts by one author', done => {
+      newPostNotification([
+        {
+          firstName: 'Danny',
+          lastName: 'Brown',
+          newPosts: [
+            { title: 'Graduating from York' },
+            { title: 'HackTrain 2.0' }
+          ]
+        }
+      ])
         .then(() => done())
         .catch(done);
-     });
-   });
- });
+    });
+
+    it('should post correct message for >2 posts by one author', done => {
+      newPostNotification([
+        {
+          firstName: 'Danny',
+          lastName: 'Brown',
+          newPosts: [
+            { title: 'Graduating from York' },
+            { title: 'HackTrain 2.0' },
+            { title: 'Centre for Computing History' }
+          ]
+        }
+      ])
+        .then(() => done())
+        .catch(done);
+    });
+
+    it('should post correct message for multiple posts by 2 authors', done => {
+      newPostNotification([
+        {
+          firstName: 'Danny',
+          lastName: 'Brown',
+          newPosts: [
+            { title: 'Graduating from York' },
+            { title: 'HackTrain 2.0' }
+          ]
+        },
+        {
+          firstName: 'Rob',
+          lastName: 'Crocombe',
+          newPosts: [
+            { title: 'Readability: Slack vs HipChat' },
+            { title: 'A walk through blog technologies' }
+          ]
+        }
+      ])
+      .then(() => done())
+      .catch(done);
+    });
+
+    it('should post correct message for multiple posts by >2 authors', done => {
+      newPostNotification([
+        {
+          firstName: 'Danny',
+          lastName: 'Brown',
+          newPosts: [
+            { title: 'Graduating from York' },
+            { title: 'HackTrain 2.0' }
+          ]
+        },
+        {
+          firstName: 'Alex',
+          lastName: 'Pringle',
+          newPosts: [
+            { title: 'Cycling out of the north' },
+            { title: 'Touring the North' },
+            { title: 'End Of The Tour' }
+          ]
+        },
+        {
+          firstName: 'Rob',
+          lastName: 'Crocombe',
+          newPosts: [
+            { title: 'Readability: Slack vs HipChat' },
+            { title: 'A walk through blog technologies' }
+          ]
+        }
+      ])
+      .then(() => done())
+      .catch(done);
+    });
+  });
+});
