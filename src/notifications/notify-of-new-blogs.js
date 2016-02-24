67927cfc524552378ed6ae29ff25c1ae4d167cc8
Daniel Brown <d.t.brown@outlook.com>
2016-02-24 20:50:35 +0000

    The notifyOfNewBlogs function shouldn't return a Promise and then within that return a promise. Fixed.

--- src/notifications/notify-of-new-blogs.js
+++ src/notifications/notify-of-new-blogs.js
@@ -25,26 +25,24 @@ function publishToNewPostTopic(subject, message) {
 }
 
 export default function notifyOfNewBlogs(usersToNewPosts) {
-  return new Promise((resolve, reject) => {
-    if (!check.array(usersToNewPosts) || usersToNewPosts.length === 0) {
-      reject(new Error('Valid array of users with new posts required'));
+  if (!check.array(usersToNewPosts) || usersToNewPosts.length === 0) {
+    throw new Error('Valid array of users with new posts required');
+  }
+
+  if (usersToNewPosts.length === 1) {
+    const singleAuthor = usersToNewPosts[0];
+    if (singleAuthor.newPosts.length === 1) {
+      return publishToNewPostTopic(`${singleAuthor.firstName} ${singleAuthor.lastName} published a new blog post`,
+                                    singleAuthor.newPosts[0].title);
     }
 
-    if (usersToNewPosts.length === 1) {
-      const singleAuthor = usersToNewPosts[0];
-      if (singleAuthor.newPosts.length === 1) {
-        return publishToNewPostTopic(`${singleAuthor.firstName} ${singleAuthor.lastName} published a new blog post`,
-                                     singleAuthor.newPosts[0].title);
-      }
+    const intro = singleAuthor.newPosts.length > 2 ? 'Including:' : '';
+    return publishToNewPostTopic(`${singleAuthor.firstName} ${singleAuthor.lastName} published ${singleAuthor.newPosts.length} new blog posts`,
+                                  `${intro} "${singleAuthor.newPosts[0].title}" and "${singleAuthor.newPosts[1].title}"`);
+  }
 
-      const intro = singleAuthor.newPosts.length > 2 ? 'Including:' : '';
-      return publishToNewPostTopic(`${singleAuthor.firstName} ${singleAuthor.lastName} published ${singleAuthor.newPosts.length} new blog posts`,
-                                    `${intro} "${singleAuthor.newPosts[0].title}" and "${singleAuthor.newPosts[1].title}"`);
-    }
-
-    const numberOfNewPosts = usersToNewPosts.reduce((total, user) => total + user.newPosts.length, 0);
-    const intro = usersToNewPosts.length > 2 ? 'Including posts by' : 'Written by';
-    return publishToNewPostTopic(`Read ${numberOfNewPosts} new posts by ${usersToNewPosts.length} authors`,
-                                 `${intro} ${usersToNewPosts[0].firstName} ${usersToNewPosts[0].lastName} and ${usersToNewPosts[1].firstName} ${usersToNewPosts[1].lastName}`);
-  });
+  const numberOfNewPosts = usersToNewPosts.reduce((total, user) => total + user.newPosts.length, 0);
+  const intro = usersToNewPosts.length > 2 ? 'Including posts by' : 'Written by';
+  return publishToNewPostTopic(`Read ${numberOfNewPosts} new posts by ${usersToNewPosts.length} authors`,
+                                `${intro} ${usersToNewPosts[0].firstName} ${usersToNewPosts[0].lastName} and ${usersToNewPosts[1].firstName} ${usersToNewPosts[1].lastName}`);
 }
