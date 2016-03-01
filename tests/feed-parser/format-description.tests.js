40dc274086d92652fd6530c390bad2b7316b5ae1
Daniel Brown <d.t.brown@outlook.com>
2016-03-01 20:18:17 +0000

    Added test for a complete real life example of a wordpress.com description

--- tests/feed-parser/format-description.tests.js
+++ tests/feed-parser/format-description.tests.js
@@ -23,5 +23,11 @@ describe('format-description', () => {
       const descriptionEndingInEllipses = 'Interesting things await [&#8230;]';
       expect(formatDescription(descriptionEndingInEllipses)).to.equal('Interesting things await');
     });
+
+    it('should parse a wordpress.com description correctly', () => {
+      const wordpressComDescription = 'On January 23rd I graduated with an MSc in Advanced Computer Science from The University of York. It was a nice occasion to get together and be merry with the family, and of course for some photos in my cap and gown &#8212; this time they were grey and blue. My parents took my degree [&#8230;]<img alt="" border="0" src="https://pixel.wp.com/b.gif?host=dannycomputerscientist.wordpress.com&#038;blog=27683847&#038;post=3840&#038;subd=dannycomputerscientist&#038;ref=&#038;feed=1" width="1" height="1" />';
+
+      expect(formatDescription(wordpressComDescription)).to.equal('On January 23rd I graduated with an MSc in Advanced Computer Science from The University of York. It was a nice occasion to get together and be merry with the family, and of course for some photos in my cap and gown — this time they were grey and blue. My parents took my degree');
+    });
   });
 });
