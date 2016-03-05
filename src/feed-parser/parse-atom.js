943cf81cc48bb09e149908eac0eb6845b47fa8e3
Daniel Brown <d.t.brown@outlook.com>
2016-03-05 15:52:24 +0000

    Added code to determine the most semantic ATOM link to store in the database. Added code to format ATOM content.

--- src/feed-parser/parse-atom.js
+++ src/feed-parser/parse-atom.js
@@ -1,9 +1,25 @@
+import formatDescription from './format-description';
+
+function getBestLink(postLinks) {
+  let preferredLink = '';
+  if (postLinks.length === 1) {
+    preferredLink = postLinks[0].href;
+  } else {
+    // Prefer alternative URIs to self URIs
+    const alternateLinks = postLinks.filter(link => link.$.rel === 'alternate').map(link => link.$.href);
+    const selfLinks = postLinks.filter(link => link.$.rel === 'self').map(link => link.$.href);
+
+    preferredLink = alternateLinks.length > 0 ? alternateLinks[0] : selfLinks[0];
+  }
+  return preferredLink;
+}
+
 export default function parseATOMPosts(parsedXML) {
   return parsedXML.feed.entry.map(entry => {
     const blogPost = {
       title: entry.title[0]._,
-      // link: entry.link,
-      // description: ''
+      link: getBestLink(entry.link),
+      description: formatDescription(entry.content[0]._)
     };
     return blogPost;
   });
