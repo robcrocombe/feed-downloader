ad2bb4a4446db4a93f8fa2912bec35152dae49b7
Daniel Brown <d.t.brown@outlook.com>
2016-03-05 11:21:02 +0000

    Wordpress.com.rss test now passes

--- src/feed-parser/extract-post-image.js
+++ src/feed-parser/extract-post-image.js
@@ -1,15 +1,22 @@
 import url from 'url';
 
-function removeImageSizeGetParamsFromURI(imageUriString) {
+function removeImageSizeGetParamsFromURI(imageURL) {
   // Wordpress tends to add image resizing get parameters. Remove them for full size image
-  const imageUrl = url.parse(imageUriString);
-  return `${imageUrl.protocol}${imageUrl.slashes ? '//' : ''}${imageUrl.host}${imageUrl.pathname}`;
+  return `${imageURL.protocol}${imageURL.slashes ? '//' : ''}${imageURL.host}${imageURL.pathname}`;
+}
+
+function isGravatar(imageURL) {
+  return (imageURL.hostname.includes('gravatar.com'));
 }
 
 export default function extractPostImage(post) {
   if (post['media:content']) {
-    // Wordpress uses media:content for images. 0th index is first or feature image
-    const imageURI = post['media:content'][0].$.url;
-    return removeImageSizeGetParamsFromURI(imageURI);
+    // Wordpress uses media:content for images. 0th index is first or feature image,
+    const imageURL = url.parse(post['media:content'][0].$.url);
+
+    // except when it's a gravatar
+    if (!isGravatar(imageURL)) {
+      return removeImageSizeGetParamsFromURI(imageURL);
+    }
   }
 }
