100b519f136c22d21e99d16c479fe90e826d46af
Daniel Brown <d.t.brown@outlook.com>
2016-03-08 20:01:25 +0000

    Updated log to know the new test command

--- src/log.js
+++ src/log.js
@@ -4,7 +4,7 @@ const log = bunyan.createLogger({
   name: 'csblogs-feed-downloader'
 });
 
-if ((process.env.npm_lifecycle_script === 'babel-node ./node_modules/istanbul/lib/cli cover node_modules/mocha/bin/_mocha -- --require tests/chai-config.js --recursive ./tests/*.tests.js')) {
+if ((process.env.npm_lifecycle_script === 'istanbul cover ./node_modules/.bin/_mocha -- --compilers js:babel-register --require tests/chai-config.js --recursive ./tests/*.tests.js')) {
   // Running in test mode. Prevent logging to stdout (makes mocha output easier to read).
   log.streams = [];
 }
