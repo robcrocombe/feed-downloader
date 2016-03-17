809ce815e9161d5923478ab176562d67e21a92c3
Daniel Brown <d.t.brown@outlook.com>
2016-03-17 20:02:21 +0000

    Updated formatDescription() to deal with new-line characters and replacements that add too many spaces. Updated tests to reflect this

--- src/feed-parser/format-description.js
+++ src/feed-parser/format-description.js
@@ -8,7 +8,14 @@ export default function formatDescription(unformattedDescription) {
     textRepresentation = textRepresentation.slice(0, textRepresentation.length - 3);
   }
 
+  /* eslint no-control-regex: 0 */ /* We do actually deal with control characters here */
   // Fix for feeds with &nbps; (NO-BREAK SPACE) characters, where we want just normal spaces
   textRepresentation = textRepresentation.replace(new RegExp('\xA0', 'g'), ' ');
+
+  // Some ATOM feeds include a new-line character, which we replace with a space
+  textRepresentation = textRepresentation.replace(new RegExp('\n', 'g'), ' ');
+
+  // The previous two replacements can result in double spacs, remove those
+  textRepresentation = textRepresentation.replace(new RegExp('  ', 'g'), ' ');
   return textRepresentation.trim();
 }
