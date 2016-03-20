ae403ae342526f25cece3bdd1742e55ffa56f04c
Daniel Brown <d.t.brown@outlook.com>
2016-03-20 03:22:21 +0000

    Added function to collate all new and modified posts and lastModifiedDates. Simplified user logging code.

--- src/aggregator/index.js
+++ src/aggregator/index.js
@@ -4,23 +4,36 @@ import log from '../log';
 import getNewPosts from './get-users-new-posts';
 import settle from 'promise-settle';
 
+function collateUpdateInformation(getNewPostResults) {
+  const newAndModifiedBlogMetadata = [];
+  getNewPostResults.forEach(result => {
+    if (result.isFulfilled()) {
+      const postMetadata = result.value();
+      if (postMetadata) {
+        newAndModifiedBlogMetadata.push(result.value());
+      }
+    } else {
+      log.warn({ error: result.reason() }, 'getUsersNewPost promise was rejected');
+    }
+  });
+  return newAndModifiedBlogMetadata;
+}
+
 export default function aggregate() {
   return new Promise((resolve, reject) => {
     database.sync()
-      .then(() =>
-        User.findAll({
+      .then(() => {
+        const users = User.findAll({
           attributes: ['id', 'authenticationProvider', 'firstName', 'lastName', 'blogFeedURI']
-        }))
-      .then(users => {
+        });
         log.info({ users }, 'Users loaded from database');
         return users;
       })
       .then(users => {
         const getUsersNewPostsPromises = users.map(user => getNewPosts(user));
-        log.info('Queued up %s get-users-new-posts promises', getUsersNewPostsPromises.length);
-        // TODO: Make transaction so that lastModifiedDates are only updated if new posts added OK
         return settle(getUsersNewPostsPromises);
       })
+      .then(collateUpdateInformation)
       .then(resolve)
       .catch(reject);
   });
