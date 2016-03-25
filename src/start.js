5718f3ae9812263370d7ebf074d5df1dac3f8db2
Daniel Brown <d.t.brown@outlook.com>
2016-03-25 23:06:41 +0000

    Add dotenv, which will load environmental variables from a file on a developers machine. For production AWS Management Console will be used.

--- src/start.js
+++ src/start.js
@@ -2,8 +2,13 @@
 // but allows you to do it from your local machine for development
 // or from a non-AWS server
 
+// Loads environmental variables using dotenv file, which would be handled by AWS otherwise
+
 import { handle } from './lambda-handle';
 import log from './log';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 handle({}, {
   succeed: result => {
